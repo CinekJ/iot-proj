95f2b56070c39b1beb3a3b0ffd700d33fd68d2cf
CinekJ <marcinkoza45@gmail.com>
2023-01-02 18:12:12 +0100

    handle Requests Recives

--- agent.py
+++ agent.py
@@ -1,7 +1,8 @@
 import json
-from azure.iot.device import IoTHubDeviceClient, Message
+from azure.iot.device import IoTHubDeviceClient, Message, MethodResponse
 import asyncio
 from asyncua import ua
+import datetime
 class Agent:
   def __init__(self, device, connection_string):
     self.device = device
@@ -9,6 +10,7 @@ class Agent:
     self.iotClient= IoTHubDeviceClient.create_from_connection_string(self.connection_string)
     self.iotClient.connect()
     self.iotClient.on_twin_desired_properties_patch_received = self.desiredPropertiesPatchReceived
+    self.iotClient.on_method_request_received = self.handleRequestRecives
     self.tasks=[]
 
   async def telemetry(self):
@@ -49,3 +51,31 @@ class Agent:
       self.tasks.append(self.device.set("ProductionRate", ua.Variant(patch["ProductionRate"], ua.VariantType.Int32)))
     if "DeviceError" in patch:
       self.tasks.append(self.device.set("DeviceError", ua.Variant(patch["DeviceError"], ua.VariantType.Int32)))
+
+  def handleRequestRecives(self, request):
+    
+    if request.name == "ResetErrors":
+      print('Reseting errors...')
+      node = self.device.client.get_node(self.device.id)
+      
+      self.tasks.append(
+        node.call_method("ResetErrorStatus")
+      )
+      self.iotClient.send_method_response(MethodResponse(request.request_id, 0))
+    
+    elif request.name == "Stop":
+      print('Emergency stop...')
+      node = self.device.client.get_node(self.device.id)
+      self.tasks.append(
+        node.call_method("EmergencyStop")
+      )
+      self.iotClient.send_method_response(MethodResponse(request.request_id, 0))
+    
+    elif request.name == "Maintenance":
+      print("Adding maintenance date...")
+      self.iotClient.patch_twin_reported_properties({"MaintenanceDate":  datetime.datetime.now().isoformat()})
+      
+      self.iotClient.send_method_response(MethodResponse(request.request_id, 0))
+
+    else:
+      print('Method not found!')
